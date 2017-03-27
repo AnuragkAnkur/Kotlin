4ed402351b6e0242338529c24d28113b6b8ff20b
anuragankur1107@gmail.com <anuragankur1107@gmail.com>
2017-03-27 15:06:25 +0100

    changed the template reference

--- .teamcity/TestCovereageThroughTeamcity_KotlinPoc/buildTypes/TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage.kt
+++ .teamcity/TestCovereageThroughTeamcity_KotlinPoc/buildTypes/TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage.kt
@@ -2,9 +2,9 @@ package TestCoverageThroughTeamCity_KotlinPoc.buildTypes
 
 import TestCovereageThroughTeamcity_KotlinPoc.vcsRoots.TestCovereageThroughTeamcity_KotlinPoc_KotlinGitVcs
 import jetbrains.buildServer.configs.kotlin.v10.*
+import jetbrains.buildServer.configs.kotlin.v10.buildSteps.script
 
 object TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage : BuildType({
-    template = "CompilePackageAndRunUnitTests"
     uuid = "7d4d6c04-a359-4799-8c3c-dc9138ee760c"
     extId = "TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage"
     name = "01 - Compile, run unit tests and package"
@@ -25,6 +25,22 @@ object TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage : B
         root(TestCovereageThroughTeamcity_KotlinPoc_KotlinGitVcs)
 
         cleanCheckout = true
+    }
+
+    steps{
+        script{
+            scriptContent = """Echo did something"""
+        }
+    }
+
+    failureConditions{
+        executionTimeoutMin = 10
+        errorMessage = true
+    }
+
+    requirements{
+        exists("DotNetFramework4.5.2_x64", "RQ_201")
+        exists("VS2015")
     }
 
     triggers {
