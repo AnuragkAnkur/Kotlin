e5210bb2f5be4d4ec788dbdd8dd4de905c858b82
anuragankur1107@gmail.com <anuragankur1107@gmail.com>
2017-04-03 16:44:16 +0100

    adds steps for creating release in octopus

--- .teamcity/TestCoverageThroughTeamcity_KotlinPoc/buildTypes/TestCoverageThroughTeamCity_KotlinPoc_02CreateReleaseInOctopus.kt
+++ .teamcity/TestCoverageThroughTeamcity_KotlinPoc/buildTypes/TestCoverageThroughTeamCity_KotlinPoc_02CreateReleaseInOctopus.kt
@@ -2,6 +2,8 @@ package TestCoverageThroughTeamcity_KotlinPoc.buildTypes
 
 import TestCoverageThroughTeamCity_KotlinPoc.buildTypes.TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage
 import jetbrains.buildServer.configs.kotlin.v10.BuildType
+import jetbrains.buildServer.configs.kotlin.v10.CheckoutMode
+import jetbrains.buildServer.configs.kotlin.v10.FailureAction
 
 /**
  * Created by AnuragA on 02/04/2017.
@@ -10,33 +12,76 @@ object TestCoverageThroughTeamCity_KotlinPoc_02CreateReleaseInOctopus : BuildTyp
     uuid = "50e5be90-f989-416f-9638-548295840cc5"
     extId = "TestCoverageThroughTeamcity_KotlinPoc_CreateReleaseInOctopus"
     name = "Create Release In Octopus"
+    buildNumberPattern = "%octopus_releasenumber%"
+
+    params{
+        param("ReleaseNotes.AdditionalParams", "")
+        param("ReleaseNotes.TargetDiffEnvironment", "Production")
+        password("ReleaseNotes.TeamCity.Password", "youni11#")
+        param("ReleaseNotes.TeamCity.Uri", "http://localhost:8111")
+        param("ReleaseNotes.TeamCity.UserId", "anuraga")
+    }
 
     vcs {
         root(TestCoverageThroughTeamcity_KotlinPoc.vcsRoots.TestCoverageThroughTeamcity_KotlinPoc_KotlinGitVcs)
-
+        checkoutMode = CheckoutMode.ON_SERVER
     }
 
     steps {
         step {
+            name = "Download Generate-ReleaseNotes nuget package"
+            type = "simpleRunner"
+            param("script.content", """C:\Teamcity\tools\NuGet.CommandLine.%Nuget.Version%\tools\NuGet.exe Install Asos.PlatformEngineering.ReleaseNotesGenerator -Source https://proget.services.kingsway.asos.com/nuget/ASOS -ExcludeVersion""")
+        }
+        step {
+            name = "Generate Release Notes"
+            type = "jetbrains_powershell"
+            param("jetbrains_powershell_bitness", "x64")
+            param("jetbrains_powershell_scriptArguments", """
+                -TeamCityURI %ReleaseNotes.TeamCity.Uri%
+                -TeamCityUserId %ReleaseNotes.TeamCity.UserId%
+                -TeamCityPassword %ReleaseNotes.TeamCity.Password%
+                -DiffTargetEnvironment "%ReleaseNotes.TargetDiffEnvironment%"
+                -OctopusURI %Octopus.Uri%
+                -octopusAPIKey %Octopus.ApiKey%
+                -octopusProject "%Octopus.Project.Name%"
+                -TeamCityProjectId "%teamcity.project.id%"
+                -BuildNumber "%env.BUILD_NUMBER%"
+                -BuildTypeId "%system.teamcity.buildType.id%"
+                %ReleaseNotes.AdditionalParams%
+            """.trimIndent())
+            param("jetbrains_powershell_script_file", """Asos.PlatformEngineering.ReleaseNotesGenerator\Generate-ReleaseNotes.ps1""")
+            param("jetbrains_powershell_script_mode", "FILE")
+        }
+        step {
             name = "Create release"
             type = "octopus.create.release"
             param("octopus_host", "http://localhost:90")
+            param("Octopus.Deployment.Timeout", "00:30:00")
             param("octopus_project_name", "Deploy HelloWorld")
             param("octopus_version", "3.0+")
             param("octopus_waitfordeployments", "true")
             param("secure:octopus_apikey", "zxx17507688bb50ccb1ddb978fa8567e24777aef946ce4abc8041eca78dd7c34de0")
+            param("octopus_releasenumber", "%dep.TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage.build.number%")
+            param("Branch.Name", "%dep.TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage.Branch.Name%")
+            param("octopus_additionalcommandlinearguments", "--deploymenttimeout=%Octopus.Deployment.Timeout% --cancelontimeout --releasenotesfile ReleaseNotes.md")
         }
     }
 
     triggers {
-        vcs {
+        trigger {
+            type = "buildDependencyTrigger"
+            param("afterSuccessfulBuildOnly", "true")
+            param("dependsOn", "TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage")
         }
     }
 
     dependencies {
         dependency(TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage)
         {
-            snapshot {  }
+            snapshot {
+                onDependencyFailure = FailureAction.FAIL_TO_START
+            }
         }
     }
 })
