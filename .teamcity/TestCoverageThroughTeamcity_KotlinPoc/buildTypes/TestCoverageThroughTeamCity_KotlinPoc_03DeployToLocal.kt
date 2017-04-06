aaff92af11c7fe29206e0f80a2812c0aec3140a0
anuragankur1107@gmail.com <anuragankur1107@gmail.com>
2017-04-06 17:28:11 +0100

    adds a build step to deployt to local

--- .teamcity/TestCoverageThroughTeamcity_KotlinPoc/buildTypes/TestCoverageThroughTeamCity_KotlinPoc_03DeployToLocal.kt
+++ .teamcity/TestCoverageThroughTeamcity_KotlinPoc/buildTypes/TestCoverageThroughTeamCity_KotlinPoc_03DeployToLocal.kt
@@ -11,15 +11,14 @@ object TestCoverageThroughTeamCity_KotlinPoc_03DeployToLocal : BuildType({
     uuid = "af24b33e-5c67-42e6-9f8b-d0065d91ab78"
     extId = "TestCoverageThroughTeamcity_KotlinPoc_DeployToLocal"
     name = "Deploy release to local"
+    buildNumberPattern = "%dep.TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage.build.number%"
 
     params {
         param("Octopus.Deploy.Environment", "local")
-        param("Octopus.Project.Name", "%dep.dep.TestCoverageThroughTeamCity_KotlinPoc_02CreateReleaseInOctopus.Octopus.Project.Name%")
         param("UpstreamBuildNumber", "%dep.TestCoverageThroughTeamCity_KotlinPoc_01CompileRunUnitTestsAndPackage.build.number%")
         param("Octopus.Deployment.Timeout", "00:30:00")
         param("Octopus.Uri", "http://localhost:90/")
         param("Api.Key", """API-VE5E6V9F7VIUKAQX8A61O2E4W""")
-
     }
 
     vcs {
@@ -33,7 +32,7 @@ object TestCoverageThroughTeamCity_KotlinPoc_03DeployToLocal : BuildType({
             type = "octopus.deploy.release"
             param("octopus_host", "%Octopus.Uri%")
             param("Octopus.Deployment.Timeout", "%Octopus.Deployment.Timeout%")
-            param("octopus_project_name", "%Octopus.Project.Name%")
+            param("Octopus.Project.Name", "Deploy HelloWorld")
             param("octopus_version", "3.0+")
             param("octopus_waitfordeployments", "true")
             param("octopus_deployto", "local")
